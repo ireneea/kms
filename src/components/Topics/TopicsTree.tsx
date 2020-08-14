02d646ccd670391fc385ecc353f140e080d98a77
irenee <irenee@qchi.net>
2020-08-14 07:45:43 +0100

    feat: use saved topics to build topics tree

--- src/components/Topics/TopicsTree.tsx
+++ src/components/Topics/TopicsTree.tsx
@@ -5,20 +5,73 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Collapse from "@material-ui/core/Collapse";
 
-import Tree, { mutateTree, moveItemOnTree, ItemId, TreeSourcePosition, TreeDestinationPosition } from "@atlaskit/tree";
+import Tree, {
+  mutateTree,
+  moveItemOnTree,
+  ItemId,
+  TreeData,
+  TreeItem,
+  TreeSourcePosition,
+  TreeDestinationPosition,
+} from "@atlaskit/tree";
 
 import TopicsTreeItem from "./TopicsTreeItem";
 
-import treeData from "./treeData";
+import useAsync from "../../hooks/useAsync";
+import { TopicsAPI } from "../../api/words";
+import { TTopic } from "../../ts/appTypes";
+
+const buildTopicsTree = (topics: TTopic[] = []): TreeData => {
+  const rootItem: TreeItem = {
+    id: "1",
+    children: [],
+    hasChildren: true,
+    isExpanded: true,
+    isChildrenLoading: false,
+    data: {
+      title: "root",
+    },
+  };
+  const tree: TreeData = {
+    rootId: rootItem.id,
+    items: {},
+  };
+
+  topics.forEach((topic) => {
+    if (topic.id) {
+      const item: TreeItem = {
+        id: topic.id,
+        children: [],
+        hasChildren: true,
+        isExpanded: true,
+        isChildrenLoading: false,
+        data: topic,
+      };
+
+      tree.items[topic.id] = item;
+      rootItem.children.push(topic.id);
+    }
+  });
+
+  tree.items[rootItem.id] = rootItem;
+
+  return tree;
+};
 
 const TopicsTree = () => {
-  const [tree, setTree] = React.useState(treeData);
+  const [tree, setTree] = React.useState(buildTopicsTree);
 
   const theme = useTheme();
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
+  const topicsLoader = useAsync(TopicsAPI.getAll, {
+    handleSuccess: (data) => setTree(buildTopicsTree(data)),
+  });
+
+  React.useEffect(() => {
+    topicsLoader.execute();
+  }, []);
 
   /** Events handlers */
-
   const handleClick = () => {
     setOpen(!open);
   };
@@ -62,8 +115,6 @@ const TopicsTree = () => {
             onCollapse={handleCollapse}
             onDragEnd={handleDragEnd}
             offsetPerLevel={theme.spacing(3)}
-            isDragEnabled
-            isNestingEnabled
           />
         </List>
       </Collapse>
